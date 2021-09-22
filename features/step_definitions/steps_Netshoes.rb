eab9043f824cf34389f28ed9c524165a4e510e35
FelipeVRR <91001245+FelipeVRR@users.noreply.github.com>
2021-09-22 11:00:20 -0300

    Update steps_Netshoes.rb

--- features/step_definitions/steps_Netshoes.rb
+++ features/step_definitions/steps_Netshoes.rb
@@ -8,7 +8,6 @@ E ('realizei uma pesquisa de {string}') do |item|
     find('input[id="search-input"]').set item
     find('button[type="submit"]').click()
 
-    sleep 10
 end
   
 Quando('seleciono o produto vou escolher o tamanho') do  
@@ -16,11 +15,14 @@ Quando('seleciono o produto vou escolher o tamanho') do
     find('img[src="//static.netshoes.com.br/produtos/tenis-infantil-nike-downshifter-11/43/HZM-5249-543/HZM-5249-543_thumb1.jpg?ts=1625514924?ims=50x"]').click()
     find('a[data-label="18.5"]').click()
     
-    sleep 10 
 end
 
   
 Entao('o mesmo será encaminhado ao carrinho') do
     find('button[id="buy-button-now"]').click()
+
+    
+    sleep 6
+
 end
   
